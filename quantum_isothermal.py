59cfa0cf0720c5efe1cb3c9b365bd4255391d477
pmocz <philip.mocz@gmail.com>
2025-12-17 14:02:18 -0500

    add notes

---
+++ quantum_isothermal.py
@@ -0,0 +1,9 @@
+import jax
+import jax.numpy as jnp
+
+# Quantum Isothermal
+
+# Philip Mocz (2025)
+
+# Simulate the compressible isothermal equations and a quantum Schrodinger-type analog equation.
+
